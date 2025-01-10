77f344cd4d3b2a70adc12d77b266d2ddff19465a
Joel Meuleman <joel.meuleman@a-cx.com>
2025-01-09 16:25:54 -0800

    Working OLA.

--- src/api/vocoder/ola_buffer.h
+++ src/api/vocoder/ola_buffer.h
@@ -2,6 +2,7 @@
 #define API_VOCODER_OLA_BUFFER_H_
 
 #include <algorithm>
+#include <iostream>
 #include <stdexcept>
 #include <vector>
 
@@ -12,52 +13,63 @@ class OLABuffer {
         kHopSize_(hop_size),
         kBufferSize_(2 * window_size),
         buffer_(kBufferSize_, 0.0f),
-        start_(0),
-        end_(0),
-        frames_before_update_start(kWindowSize_ / kHopSize_) {}
+        index_(0) {}
 
   std::vector<float> OverlapAdd(const std::vector<float>& samples) {
     if (samples.size() != kWindowSize_) {
       throw std::runtime_error("Samples size must be equal to window size");
     }
 
-    // Overlap-add samples.
-    for (int i = 0; i < kWindowSize_; ++i) {
-      buffer_[(end_ + i) % kBufferSize_] += samples[i];
+    // Overlap add the samples to the buffer.
+    for (size_t i = 0; i < kWindowSize_; ++i) {
+      buffer_[index_ + i] += samples[i];
+    }
+    index_ += kHopSize_;
+
+    std::cout << "Added\n";
+    for (const auto item : buffer_) {
+      std::cout << item << " ";
+    }
+    std ::cout << " | " << index_ << " | ";
+    std::cout << std::endl;
+
+    // Copy the output frame from the buffer.
+    const std::vector<float> kOutput(buffer_.begin(),
+                                     buffer_.begin() + kWindowSize_);
+
+    // If the index has moved beyond window size, we know we have a completed
+    // frame and should shift the buffer, clearing the portion of the buffer
+    // that does not contain a partially completed frame.
+    if (index_ >= kWindowSize_) {
+      // Instead of copy, use std::memmove.
+      // std::copy(buffer_.begin() + kHopSize_, buffer_.end(), buffer_.begin());
+      std::memmove(buffer_.data(), buffer_.data() + kHopSize_,
+                   (kBufferSize_ - kHopSize_) * sizeof(float));
+
+      std::cout << "Copied\n";
+      for (const auto item : buffer_) {
+        std::cout << item << " ";
+      }
+      std::cout << std::endl;
+
+      std::fill(buffer_.end() - kHopSize_, buffer_.end(), 0.0f);
+
+      index_ -= kHopSize_;
+
+      std::cout << "Filled\n";
+      for (const auto item : buffer_) {
+        std::cout << item << " ";
+      }
+      std::cout << std::endl;
     }
 
-    // Update the start and end indices.
-    end_ = (end_ + kHopSize_) % kBufferSize_;
-    if (frames_before_update_start == 0) {
-      start_ = (start_ + kHopSize_) % kBufferSize_;
-    } else {
-      --frames_before_update_start;
-    }
-
-    // Collect the output frame.
-    std::vector<float> output_frame(kWindowSize_);
-    for (int i = 0; i < kWindowSize_; ++i) {
-      output_frame[i] = buffer_[(start_ + i) % kBufferSize_];
-    }
-
-    return output_frame;
+    return kOutput;
   }
 
  private:
   const size_t kWindowSize_, kHopSize_, kBufferSize_;
   std::vector<float> buffer_;
-
-  // Positions in the ring buffer. Start only begins to move after sufficient
-  // frames have been added such that all frames overlapping the start frame are
-  // present.
-  size_t start_, end_;
-
-  // The number of frames required before the start position can be updated is
-  // given by: WindowSize / HopSize.
-  // NOTE: This math is incorrect, it's most likely ceil(WindowSize / HopSize)
-  // but I'll need to ensure that operation won't overwrite the head of the
-  // buffer.
-  size_t frames_before_update_start;
+  size_t index_;
 };
 
 #endif  // API_VOCODER_OLA_BUFFER_H_
--- src/api/vocoder/test/ola_buffer_test.cc
+++ src/api/vocoder/test/ola_buffer_test.cc
@@ -14,8 +14,61 @@ TEST(OLABufferTest, OverlapAdd) {
   EXPECT_EQ(buffer.OverlapAdd(samples), std::vector<float>({2.0f, 3.0f, 3.0f}));
   EXPECT_EQ(buffer.OverlapAdd(samples), std::vector<float>({3.0f, 3.0f, 3.0f}));
 
-  // for (int i = 0; i < 10; ++i) {
-  //   EXPECT_EQ(buffer.OverlapAdd(samples),
-  //             std::vector<float>({3.0f, 3.0f, 3.0f}));
-  // }
+  for (int i = 0; i < 10; ++i) {
+    EXPECT_EQ(buffer.OverlapAdd(samples),
+              std::vector<float>({3.0f, 3.0f, 3.0f}));
+  }
+}
+
+TEST(OLABufferTest, OverlapAddDifferentWindowSize) {
+  const size_t window_size = 4;
+  const size_t hop_size = 2;
+  const std::vector<float> samples = {1.0f, 1.0f, 1.0f, 1.0f};
+  OLABuffer buffer(window_size, hop_size);
+
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({1.0f, 1.0f, 1.0f, 1.0f}));
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({1.0f, 1.0f, 2.0f, 2.0f}));
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({2.0f, 2.0f, 2.0f, 2.0f}));
+
+  for (int i = 0; i < 10; ++i) {
+    EXPECT_EQ(buffer.OverlapAdd(samples),
+              std::vector<float>({2.0f, 2.0f, 2.0f, 2.0f}));
+  }
+}
+
+TEST(OLABufferTest, OverlapAddDifferentHopSize) {
+  const size_t window_size = 4;
+  const size_t hop_size = 3;
+  const std::vector<float> samples = {1.0f, 1.0f, 1.0f, 1.0f};
+  const std::vector<std::vector<float>> results = {
+      {1.f, 1.f, 1.f, 1.f},
+      {1.f, 1.f, 1.f, 2.f},
+      {2.f, 1.f, 1.f, 2.f},
+      {2.f, 1.f, 1.f, 2.f},
+
+  };
+  OLABuffer buffer(window_size, hop_size);
+
+  for (const auto& result : results) {
+    EXPECT_EQ(buffer.OverlapAdd(samples), result);
+  }
+}
+
+TEST(OLABufferTest, NoOverlapAdd) {
+  const size_t window_size = 4;
+  const size_t hop_size = 4;
+  const std::vector<float> samples = {1.0f, 1.0f, 1.0f, 1.0f};
+  OLABuffer buffer(window_size, hop_size);
+
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({1.0f, 1.0f, 1.0f, 1.0f}));
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({1.0f, 1.0f, 1.0f, 1.0f}));
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({1.0f, 1.0f, 1.0f, 1.0f}));
+  EXPECT_EQ(buffer.OverlapAdd(samples),
+            std::vector<float>({1.0f, 1.0f, 1.0f, 1.0f}));
 }
